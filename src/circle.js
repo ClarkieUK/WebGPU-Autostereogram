7e2a4927a0d59a48c7364a6a67927833ab6a2b6b
ClarkieUK <johndoe@email.com>
2026-01-11 17:34:47 +0000

    Vertex Buffers When Colors Are 4 * 4 Bytes

--- src/circle.js
+++ src/circle.js
@@ -47,4 +47,120 @@ function createCircleVertices({
   };
 }
 
-export { createCircleVertices }
+function createColoredCircleVertices({
+  radius = 1,
+  numSubdivisions = 24,
+  innerRadius = 0,
+  startAngle = 0,
+  endAngle = Math.PI * 2,
+} = {}) {
+  // 2 triangles per subdivision, 3 verts per tri, 5 values (xyrgb) each.
+  const numVertices = numSubdivisions * 3 * 2;
+  const vertexData = new Float32Array(numVertices * (2 + 3));
+ 
+  let offset = 0;
+  const addVertex = (x, y, r, g, b) => {
+    vertexData[offset++] = x;
+    vertexData[offset++] = y;
+    vertexData[offset++] = r;
+    vertexData[offset++] = g;
+    vertexData[offset++] = b;
+  };
+ 
+  const innerColor = [1, 1, 1];
+  const outerColor = [0.1, 0.1, 0.1];
+ 
+  // 2 triangles per subdivision
+  //
+  // 0--1 4
+  // | / /|
+  // |/ / |
+  // 2 3--5
+  for (let i = 0; i < numSubdivisions; ++i) {
+    const angle1 = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+    const angle2 = startAngle + (i + 1) * (endAngle - startAngle) / numSubdivisions;
+ 
+    const c1 = Math.cos(angle1);
+    const s1 = Math.sin(angle1);
+    const c2 = Math.cos(angle2);
+    const s2 = Math.sin(angle2);
+ 
+    // first triangle
+    addVertex(c1 * radius, s1 * radius, ...outerColor);
+    addVertex(c2 * radius, s2 * radius, ...outerColor);
+    addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+ 
+    // second triangle
+    addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+    addVertex(c2 * radius, s2 * radius, ...outerColor);
+    addVertex(c2 * innerRadius, s2 * innerRadius, ...innerColor);
+  }
+ 
+  return {
+    vertexData,
+    numVertices,
+  };
+}
+
+function createNormalisedCircleVertices({
+  radius = 1,
+  numSubdivisions = 24,
+  innerRadius = 0,
+  startAngle = 0,
+  endAngle = Math.PI * 2,
+} = {}) {
+  // 2 triangles per subdivision, 3 verts per tri
+  const numVertices = numSubdivisions * 3 * 2;
+  // 2 32-bit values for position (xy) and 1 32-bit value for color (rgb_)
+  // The 32-bit color value will be written/read as 4 8-bit values
+  const vertexData = new Float32Array(numVertices * (2 + 1));
+  const colorData = new Uint8Array(vertexData.buffer);
+ 
+  let offset = 0;
+  let colorOffset = 8;
+  const addVertex = (x, y, r, g, b) => {
+    vertexData[offset++] = x;
+    vertexData[offset++] = y;
+    offset += 1;  // skip the color
+    colorData[colorOffset++] = r * 255;
+    colorData[colorOffset++] = g * 255;
+    colorData[colorOffset++] = b * 255;
+    colorOffset += 9;  // skip extra byte and the position
+  };
+ 
+  const innerColor = [1, 1, 1];
+  const outerColor = [0.1, 0.1, 0.1];
+ 
+  // 2 triangles per subdivision
+  //
+  // 0--1 4
+  // | / /|
+  // |/ / |
+  // 2 3--5
+  for (let i = 0; i < numSubdivisions; ++i) {
+    const angle1 = startAngle + (i + 0) * (endAngle - startAngle) / numSubdivisions;
+    const angle2 = startAngle + (i + 1) * (endAngle - startAngle) / numSubdivisions;
+ 
+    const c1 = Math.cos(angle1);
+    const s1 = Math.sin(angle1);
+    const c2 = Math.cos(angle2);
+    const s2 = Math.sin(angle2);
+ 
+    // first triangle
+    addVertex(c1 * radius, s1 * radius, ...outerColor);
+    addVertex(c2 * radius, s2 * radius, ...outerColor);
+    addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+ 
+    // second triangle
+    addVertex(c1 * innerRadius, s1 * innerRadius, ...innerColor);
+    addVertex(c2 * radius, s2 * radius, ...outerColor);
+    addVertex(c2 * innerRadius, s2 * innerRadius, ...innerColor);
+  }
+ 
+  return {
+    vertexData,
+    numVertices,
+  };
+}
+
+export { createCircleVertices, createColoredCircleVertices, createNormalisedCircleVertices}
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import code from './shaders/fundamentals.wgsl?raw'; // this works fine with vite
 import { initWebGPU } from './utils/initWebGPU';
 import { generateObserverCallback } from './utils/initWebGPU';
 import { rand } from './utils/randomNumber';
-import { createCircleVertices } from './circle';
+import { createCircleVertices , createColoredCircleVertices} from './circle';
 
 async function main()
 {
@@ -23,11 +23,27 @@ async function main()
             module,
             buffers: [
                 {
-                    arrayStride: 2 * 4, 
+                    arrayStride: 5 * 4, // vertex positions
                     attributes: [
                         {shaderLocation: 0, offset: 0, format: 'float32x2'},
+                        {shaderLocation: 4, offset: 8, format: 'float32x3'}
                     ]
-                }
+                },
+                {
+                    arrayStride: 6 * 4, // color (4) + offset (2)
+                    stepMode: 'instance', // every draw call increment by stride so the entire triangle
+                    attributes: [         // vertice set has these values, instead of per vertex data
+                        {shaderLocation: 1, offset: 0, format: 'float32x4'},
+                        {shaderLocation: 2, offset: 16, format: 'float32x2'}
+                    ]
+                },
+                {
+                    arrayStride: 2 * 4, // scale positions
+                    stepMode: 'instance',
+                    attributes: [
+                        {shaderLocation: 3, offset: 0, format: 'float32x2'},
+                    ]
+                },
             ]
         },
         fragment: {
@@ -39,71 +55,65 @@ async function main()
     const kNumObjects = 5;
     const objectInfos = [];
 
+    const kColorOffset = 0;
+    const kOffsetOffset = 4;
+
+    const kScaleOffset = 0;
+
     const staticUnitSize = 
     4 * 4 +
-    2 * 4 + 
-    2 * 4; // per primative
+    2 * 4; // 4 values of 4 bytes + 2 values of 4 bytes
 
     const dynamicUnitSize = 
-    2 * 4 +
-    2 * 4 ;
+    2 * 4;
 
-    const staticStorageBufferSize = staticUnitSize * kNumObjects;
-    const dynamicStorageBufferSize = dynamicUnitSize * kNumObjects;
-    
-    const kColorOffset = 0;  // color first
-    const kOffsetOffset = 4; // after 4 values
+    const staticVertexBufferSize = staticUnitSize * kNumObjects;
+    const dynamicVertexBufferSize = dynamicUnitSize * kNumObjects;
 
-    const kScaleOffset = 0; 
-
-    const { vertexData, numVertices } = createCircleVertices({
+    // Vertex Data
+    const { vertexData, numVertices } = createColoredCircleVertices({
     radius: 0.5,
     innerRadius: 0.25,
     });
 
     const vertexBuffer = device.createBuffer({
-    label: 'storage buffer vertices',
+    label: 'vertices',
     size: vertexData.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
     });
+
     device.queue.writeBuffer(vertexBuffer, 0, vertexData);
 
-    const staticStorageBuffer = device.createBuffer({
+
+    // Color / Offset Data
+    const staticVertexValues = new Float32Array(staticVertexBufferSize / 4);
+
+    const staticVertexBuffer = device.createBuffer({
         label:'static',
-        size: staticStorageBufferSize, // will be 4 values of 4 bytes + 2 values of 4 bytes + 2 values of padding of 4 bytes * number of objects
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        size: staticVertexBufferSize, // will be 4 values of 4 bytes + 2 values of 4 bytes + 2 values of padding of 4 bytes * number of objects
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
     });
-
-    const dynamicStorageBuffer = device.createBuffer({
-        label:'dynamic',
-        size: dynamicStorageBufferSize,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-    });
-
-    const staticStorageValues = new Float32Array(staticStorageBufferSize / 4);
 
     for (let i = 0; i < kNumObjects; ++i) {
         const staticOffset = i * (staticUnitSize / 4); // 0, 8 , 16 , 24
                                                        // 1cr,1cg,1cb,1ca,1px,1py,1pp,1pp,...
-        staticStorageValues.set([rand(), rand(), rand(), 1], staticOffset + kColorOffset);        // set the color
-        staticStorageValues.set([rand(-0.9, 0.9), rand(-0.9, 0.9)], staticOffset + kOffsetOffset);      // set the offset
+        staticVertexValues.set([rand(), rand(), rand(), 1], staticOffset + kColorOffset);        // set the color
+        staticVertexValues.set([rand(-0.9, 0.9), rand(-0.9, 0.9)], staticOffset + kOffsetOffset);      // set the offset
  
         objectInfos.push({
             scale: rand(0.2, 0.5),
         });
     }
-    device.queue.writeBuffer(staticStorageBuffer, 0, staticStorageValues);
+    device.queue.writeBuffer(staticVertexBuffer, 0, staticVertexValues);
 
-    const storageValues = new Float32Array(dynamicStorageBufferSize / 4);
+    // Scale Data
+    const vertexScaleValues = new Float32Array(dynamicVertexBufferSize / 4);
 
-    const bindGroup = device.createBindGroup({
-        label: 'b1',
-        layout: pipeline.getBindGroupLayout(0),
-        entries: [
-            { binding: 0, resource: { buffer: staticStorageBuffer }},
-            { binding: 1, resource: { buffer: dynamicStorageBuffer }},
-        ]
-    })
+    const dynamicVertexBuffer = device.createBuffer({
+        label:'dynamic',
+        size: dynamicVertexBufferSize,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
 
     const renderPassDescriptor = {
         colorAttachments: [
@@ -131,13 +141,15 @@ async function main()
 
         objectInfos.forEach(({scale}, ndx) => {
             const offset = ndx * (dynamicUnitSize / 4);
-            storageValues.set([scale / aspect, scale], offset + kScaleOffset);
+            vertexScaleValues.set([scale / aspect, scale], offset + kScaleOffset);
         });
 
-        device.queue.writeBuffer(dynamicStorageBuffer, 0, storageValues);
+        device.queue.writeBuffer(dynamicVertexBuffer, 0, vertexScaleValues);
         
-        pass.setBindGroup(0, bindGroup);
         pass.setVertexBuffer(0, vertexBuffer);
+        pass.setVertexBuffer(1, staticVertexBuffer);
+        pass.setVertexBuffer(2, dynamicVertexBuffer);
+
         pass.draw(numVertices, kNumObjects);
 
         pass.end();
